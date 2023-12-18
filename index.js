4f00e04c81e51ed50009cdca677a74f30bc987b2
Klif <sang.wicklife@student.moringaschool.com>
2023-12-18 07:33:01 +0300

    Assignment completed

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callBackF) {
+    callBackF();
+}
+
+function returnsANamedFunction() {
+    return function name() {
+        
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        
+    }
+}
